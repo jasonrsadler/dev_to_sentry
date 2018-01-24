4cd35888cd161d8bdfe5e528e860d19ceb8073b7
Jason Sadler <jason.r.sadler@gmail.com>
2018-01-24 13:20:40 -0500

    refined home pages

--- dev_to_sentry/src/containers/Home.js
+++ dev_to_sentry/src/containers/Home.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
+import Devs from '../containers/Devs';
+import How from '../containers/How';
 
 class Home extends Component {
     render() {
         return (
-            <section id='Home' className='home-split-container'>
-            <div className='site-title home-left-col'>
-                <article className='home-split-article'>
-                    <span className='home-text'>Find the team to travel your road</span>
-                </article>
+            <div>
+                <main className='wrapper'>
+                    <section id='Home' className='home-split-container parallax img-bg'>
+                        <div className='site-title home-left-col'>
+                            <article className='home-split-article'>
+                                <span className='home-text'>Find the team to travel your road</span>
+                            </article>
+                        </div>
+                        <div className='home-right-col'>
+                            <article className='split-article-tagline'>
+                                <span className='home-text-right'>DSentr</span>
+                                <span className='home-tagline'>Bringing Devs and Entrepenuers Together</span>
+                            </article>
+                        </div>
+                    </section>
+                    <section id="section-1" className="split-container" name="split">
+                        <Devs />
+                        <How />                    
+                    </section>   
+                </main>
+                         
             </div>
-            <div className='home-right-col img-bg'>
-                <article className='split-article-tagline'>
-                    <span className='home-text-right'>DSentr</span>
-                    <span className='home-tagline'>Bringing Devs and Entrepenuers Together</span>
-                </article>
-            </div>
-            </section>
+            
         );
     }
 }
